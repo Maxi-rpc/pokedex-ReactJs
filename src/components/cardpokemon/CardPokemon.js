9cfbe4db886fdf9b3db99dcc635c00c6644f4d00
Maximiliano <maxirpc2607@gmail.com>
2022-12-02 19:55:48 -0300

    add components
    se agrega modal para view de propiedades del pokemon

--- src/components/cardpokemon/CardPokemon.js
+++ src/components/cardpokemon/CardPokemon.js
@@ -1,42 +1,16 @@
 import { Card, Col } from "react-bootstrap";
 
-const pokemon = {
-	id: 132,
-	name: "ditto",
-	sprites: {
-		front_default:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
-		front_shiny:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png",
-		other: {
-			home: {
-				front_default:
-					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png",
-				front_shiny:
-					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/132.png",
-			},
-			"official-artwork": {
-				front_default:
-					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png",
-			},
-		},
-	},
-};
-
-export default function CardPokemon() {
+export default function CardPokemon({ name, sprites }) {
 	return (
 		<>
-			<Col className="col-3 col-md-4">
-				<Card style={{ width: "18rem" }}>
-					<Card.Body>
+			<Col className="col-3 col-md-2 my-3">
+				<Card>
+					<Card.Body className="p-0">
 						<Card.Img
 							variant="top"
-							src={pokemon.sprites.front_default}
-							alt={pokemon.name}
+							src={sprites.front_default}
+							alt={name}
 						></Card.Img>
-						<div>
-							<h4>CardPokemon</h4>
-						</div>
 					</Card.Body>
 				</Card>
 			</Col>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,46 @@
 import Template from "../template/Template";
 import { ContainerPokemon, CardPokemon } from "../../components/index";
 
+const pokemon = {
+	id: 132,
+	name: "ditto",
+	sprites: {
+		front_default:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+		front_shiny:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png",
+		other: {
+			home: {
+				front_default:
+					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png",
+				front_shiny:
+					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/132.png",
+			},
+			"official-artwork": {
+				front_default:
+					"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png",
+			},
+		},
+	},
+};
+
 export default function Home() {
+	let lista = [];
+	for (let index = 0; index < 12; index++) {
+		lista.push(pokemon);
+	}
+
 	return (
 		<>
 			<Template>
 				<div>Home</div>
 				<ContainerPokemon>
-					<CardPokemon></CardPokemon>
+					{lista.map((singlePokemon) => (
+						<CardPokemon
+							name={singlePokemon.name}
+							sprites={singlePokemon.sprites}
+						></CardPokemon>
+					))}
 				</ContainerPokemon>
 			</Template>
 		</>
