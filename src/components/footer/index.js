5ebf12e7d59d03eb937cd122bef6589629fa9262
Maximiliano <maxirpc2607@gmail.com>
2023-08-29 18:51:59 -0300

    Update index.js
    fix bottom footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 // bootstrap
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Navbar, Container } from "react-bootstrap";
 // config
 import Config from "../../config";
 
 export default function FooterComponent() {
 	return (
 		<>
-			<Row className="justify-content-center bg-dark text-light py-3">
-				<Col md="6" className="text-center">
-					<p className="mb-0">Creado por {Config.creator}</p>
-				</Col>
-			</Row>
+			<Navbar fixed="bottom" expand="lg" className="bg-dark text-light">
+				<Container fluid className="justify-content-center">
+					<Row className="py-3">
+						<Col md="12" className="text-center">
+							<p className="mb-0">Creado por {Config.creator}</p>
+						</Col>
+					</Row>
+				</Container>
+			</Navbar>
 		</>
 	);
 }
